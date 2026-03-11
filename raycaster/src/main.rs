37493d4435cb0b17e6c39f087c50be408b5a7efb
Feydral <lian.hable.feathertech@gmail.com>
2026-03-11 14:32:06 +0100

    Fix bugs

--- raycaster/src/main.rs
+++ raycaster/src/main.rs
@@ -23,6 +23,7 @@ fn main() {
     
     let mut player_pos = Float2::new(3.5, 2.5);
     let mut view_angle = 0.0;
+    let fov: f32 = 60.0;
 
     
     loop {
@@ -50,17 +51,33 @@ fn main() {
         if input.is_key_pressed(KeyCode::Right) {
             player_pos.x -= 0.05;
         }
-        
-        let distances = raycaster::cast_rays(player_pos, view_angle, 80.0_f32.to_radians(), canvas.width(), &map);
+
+        let distances = raycaster::cast_rays(player_pos, view_angle, fov.to_radians(), canvas.width(), &map);
+
+        let projection_dist = (canvas.width() as f32 / 2.0) / (fov / 2.0 * std::f32::consts::PI / 180.0).tan();
+        let screen_height = canvas.height() as f32;
+        let screen_center = screen_height / 2.0;
 
         for x in 0..canvas.width() {
-            let distance = distances[x as usize];
-            let wall_height = (1.0 / distance * 60.0).min(canvas.height() as f32) as u32;
+            for y in 0..canvas.height() {
+                if y > canvas.height() / 2 {
+                    canvas.set_pixel(x, y, Color::new(230, 230, 230));
+                } else {
+                    canvas.set_pixel(x, y, Color::new(30, 30, 30));
+                }
+            }
 
-            let canvas_center = canvas.height() / 2;
-            let wall_start = canvas_center - (wall_height / 2);
-            let wall_end = wall_start + wall_height;
-                
+            let distance = distances[x as usize].max(0.05);
+
+            let wall_height_f = projection_dist / distance;
+            let wall_height_f = wall_height_f.min(screen_height * 2.0);
+
+            let wall_start_f = screen_center - (wall_height_f / 2.0);
+            let wall_end_f = wall_start_f + wall_height_f;
+
+            let wall_start = wall_start_f.max(0.0) as u32;
+            let wall_end = wall_end_f.min(screen_height) as u32;
+
             for y in wall_start..wall_end {
                 canvas.set_pixel(x, y, Color::GRAY);
             }
